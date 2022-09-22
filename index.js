b075b797f2b8e1d4a202715dfcd1439c9b89a4aa
Lausofi <laura.sobrero87@gmail.com>
2022-09-21 23:12:47 -0300

    tieneerrores

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 let opcion
+let arrayTareas =['primero' , 'segundo' , 'tercero']
 
 do{
     opcion = parseInt(prompt('Escriba la opcion que desea: \n 1. Indicar tarea \n 2. Eliminar tarea \n 3. Salir'))
 
     switch (opcion){
         case 1:
-            alert('Indicar tarea'); {
+            alert('Indicar tarea'); 
+
                 let tareas = prompt("Describa: ")
+                arrayTareas.push(tarea.toLocaleLowercase())
                 console.log(tareas)
-            }
+            
             break
 
         case 2:
-            alert('Eliminar tarea'); {
-                let arrayTareas = ["tender la cama", "pasear al perro"];
-                arrayTareas.pop();
-                console.log(arrayTareas)
+            if(arrayTareas.length === 0) {alert('No tiene tareas creadas'); break;}
+            alert("Eliminar una tarea:"
+                ${arrayTareas.map(item=> item).join("-")},
+                "por favor ingrese el titulo a eliminar");
+
+
+            let titulo = prompt("Titulo a Eliminar: ").toLocaleLowerCase();
+
+            if(arrayTareas.some(item => item === titulo)){
+                arrayTareas = arrayTareas.filter(item => item != titulo)
             }
+            else{alert('El titulo ingresado no existe'); break;}
+            console.log(arrayTareas);
+
             break
 
         case 3:
@@ -28,4 +40,5 @@ do{
 
     }
 
+        
 }while(option != 4)
