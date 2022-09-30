4c854466ca7916661357965b762d8f5601008a05
Lausofi <laura.sobrero87@gmail.com>
2022-09-29 23:08:21 -0300

    modificacion

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
-const addForm = document.querySelector('.add');
- 
+const addForm =document.getElementById('formulario');
+const container = document.getElementById('contenedor-de-cards');
+const div = document.createElement('div');
+
+let listaDeTareas = []
+
+function Tarea(id,titulo , responsable, tiempo){
+    this.id = id;
+    this.titulo = titulo;
+    this.responsable = responsable;
+    this.tiempo = tiempo;
+}
+
 addForm.addEventListener('submit', e => {
     e.preventDefault();
-    const newToDo = addForm.add.value;
-    console.log(newToDo);
+    console.log('submit');
+
+    listaDeTareas.push(new Tarea(
+        listaDeTareas.length +1,
+        document.getElementById('input-titulo').value,
+        document.getElementById('input-responsable').value,
+        document.getElementById('input-tiempo').value,
+    ))
+
+    generateTemplate();
 });
 
-const newToDo = addForm.add.value.trim();
+function generateTemplate(){
+    div.classList.add('container');
+    div.classList.add('col-5');
+    div.classList.add('g-3');
+    div.innerHTML = ''
+    listaDeTareas.forEach((item)=>{
+        div.innerHTML += `
+        <div id="card-${item.id}" class="card" style="width: 18rem; color: black">
+            <div class="card-body">
+                <h5 class="card-title">${item.titulo} ID#${item.id}</h5>
+                <p class="card-text">Esta tarea tiene como responsable a ${item.responsable} y requiere un tiempo de ${item.tiempo} horas</p>
+                <a class="btn btn-danger" onclick="eliminarCard(${item.id})">Eliminar</a>
+            </div>
+        </div>
+        `
+    })
+    container.appendChild(div)
+}
 
-const list = document.querySelector('.to-dos');
-const generateTemplate = toDo => {
-    const html = `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${toDo}</span>
-        <i class="far fa-trash-alt delete"></i>
-    </li>
-    `;
-    list.innerHTML += html;
-};
-addForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const newToDo = addForm.add.value.trim();
-    generateTemplate(newToDo);
-});
-
-if(newToDo.length) {
-    generateTemplate(newToDo);
-    addForm.reset();
+function eliminarCard(idFormDelete){
+    listaDeTareas = listaDeTareas.filter((item)=> item.id !== idFormDelete)
+    div.removeChild(document.getElementById(`card-${idFormDelete}`))
+    generateTemplate();
 }
